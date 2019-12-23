d55eb80f48aa5597ef9f0abd20e01d62e7401bae
pftom <1043269994@qq.com>
2019-12-23 22:09:30 +0800

    来一打页面

--- src/app.jsx
+++ src/app.jsx
@@ -10,25 +10,36 @@ import './app.css'
 // }
 
 class App extends Component {
-
   config = {
-    pages: [
-      'pages/index/index'
-    ],
+    pages: ['pages/index/index', 'pages/mine/mine', 'pages/post/post'],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: 'WeChat',
-      navigationBarTextStyle: 'black'
-    }
+      navigationBarTextStyle: 'black',
+    },
+    tabBar: {
+      list: [
+        {
+          pagePath: 'pages/index/index',
+          text: '首页',
+          iconPath: './images/home.png',
+          selectedIconPath: './images/homeSelected.png',
+        },
+        {
+          pagePath: 'pages/mine/mine',
+          text: '我的',
+          iconPath: './images/mine.png',
+          selectedIconPath: './images/mineSelected.png',
+        },
+      ],
+    },
   }
 
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
   render() {
-    return (
-      <Index />
-    )
+    return <Index />
   }
 }
 
--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -4,9 +4,20 @@ import { View } from '@tarojs/components'
 import './index.css'
 
 export default class PostCard extends Component {
+  handleClick = () => {
+    const { isList } = this.props
+
+    // 如果是列表，那么就响应点击事件，跳转到帖子详情
+    if (isList) {
+      const { title, content } = this.props
+      Taro.navigateTo({
+        url: `/pages/post/post?title=${title}&content=${content}`,
+      })
+    }
+  }
   render() {
     return (
-      <View className="postcard">
+      <View className="postcard" onClick={this.handleClick}>
         <View className="post-title">{this.props.title}</View>
         <View className="post-content">{this.props.content}</View>
       </View>
