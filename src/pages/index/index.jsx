cbc2e44c9766dc0a7fed96735f61c8fd2a05e07d
pftom <1043269994@qq.com>
2019-12-23 18:43:57 +0800

    Hooks 轻装上阵

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,38 +1,43 @@
-import Taro, { Component } from '@tarojs/taro'
+import Taro, { useState } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import { PostCard, PostForm } from '../../components'
 import './index.css'
 
-export default class Index extends Component {
-  config = {
-    navigationBarTitleText: '首页'
+export default function Index() {
+  const [posts, setPosts] = useState([
+    {
+      title: '泰罗奥特曼',
+      content: '泰罗是奥特之父和奥特之母唯一的亲生儿子。',
+    },
+  ])
+  const [formTitle, setFormTitle] = useState('')
+  const [formContent, setFormContent] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const newPosts = posts.concat({ title: formTitle, content: formContent })
+    setPosts(newPosts)
+    setFormTitle('')
+    setFormContent('')
   }
 
-  state = {
-    posts: [
-      {
-        title: '泰罗奥特曼',
-        content: '泰罗是奥特之父和奥特之母唯一的亲生儿子。',
-      },
-    ],
-    formTitle: '',
-    formContent: '',
-  }
+  return (
+    <View className="index">
+      {posts.map((post, index) => (
+        <PostCard key={index} title={post.title} content={post.content} />
+      ))}
+      <PostForm
+        formTitle={formTitle}
+        formContent={formContent}
+        handleSubmit={e => handleSubmit(e)}
+        handleTitleInput={e => setFormTitle(e.target.value)}
+        handleContentInput={e => setFormContent(e.target.value)}
+      />
+    </View>
+  )
+}
 
-  render() {
-    return (
-      <View className="index">
-        {this.state.posts.map((post, index) => (
-          <PostCard key={index} title={post.title} content={post.content} />
-        ))}
-        <PostForm
-          formTitle={this.state.formTitle}
-          formContent={this.state.formContent}
-          handleSubmit={e => this.handleSubmit(e)}
-          handleTitleInput={e => this.handleTitleInput(e)}
-          handleContentInput={e => this.handleContentInput(e)}
-        />
-      </View>
-    )
-  }
+Index.config = {
+  navigationBarTitleText: '首页',
 }
